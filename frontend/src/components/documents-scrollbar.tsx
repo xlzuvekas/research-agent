7e9a0c41751045392c93f3d154655eb2e8019f26
Tyler Slaton <tyler@copilotkit.ai>
2024-12-18 18:47:31 -0500

    style: make document scrollbar always open
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

--- frontend/src/components/documents-scrollbar.tsx
+++ frontend/src/components/documents-scrollbar.tsx
@@ -15,10 +15,7 @@ export function DocumentsScrollbar({
     selectedSectionId
 }: DocumentsScrollbarProps) {
     return (
-        <div className="group fixed right-0 top-0 h-full transition-transform duration-300 transform translate-x-full hover:translate-x-0 z-10">
-            <div className="absolute -left-[31px] top-1/2 -translate-y-1/2 bg-[#FAF9F6] p-1 rounded-l-lg border border-black/10 cursor-pointer z-20 border-r-0">
-                <ChevronLeft className="group-hover:rotate-180 transition-transform duration-500" />
-            </div>
+        <div className="group h-full z-10">
             <div className="p-4 overflow-y-auto space-y-2 flex flex-col overflow-x-hidden border border-black/10 bg-[#FAF9F6] h-full relative">
                 {sections?.map(section => (
                     <DocumentViewer
